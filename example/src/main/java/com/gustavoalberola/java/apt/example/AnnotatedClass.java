d873a49f99fa33bd8ad9fb8b6ca4eab3d76bd630
Gustavo Alberola <alberolagustavo@gmail.com>
2013-11-01 17:02:57 -0300

    Removed main entry point

--- example/src/main/java/com/gustavoalberola/java/apt/example/AnnotatedClass.java
+++ example/src/main/java/com/gustavoalberola/java/apt/example/AnnotatedClass.java
@@ -4,10 +4,6 @@ import com.gustavoalberola.java.apt.annotation.SimpleAnnotation;
 
 public class AnnotatedClass {
 	
-	static public void main(String[] args) {
-		(new AnnotatedClass()).foo();
-	}
-	
 	@SimpleAnnotation
 	public void foo() {
 		System.out.println("Im the foo master");		
