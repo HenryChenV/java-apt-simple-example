cd42fd5a4e8588221aee353b8f62a739f201ea48
Gustavo Alberola <alberolagustavo@gmail.com>
2013-11-01 17:04:13 -0300

    Updated logic in Annotation Processor

--- annotationProcessor/src/main/java/com/gustavoalberola/java/apt/annotationprocessor/SimpleAnnotationProcessor.java
+++ annotationProcessor/src/main/java/com/gustavoalberola/java/apt/annotationprocessor/SimpleAnnotationProcessor.java
@@ -33,48 +33,64 @@ public class SimpleAnnotationProcessor extends AbstractProcessor {
 			for (Element implementatorElement : elementsAnnotatedWith) {
 				
 				if (implementatorElement.getKind() == ElementKind.METHOD) {
-					Element classElement = implementatorElement.getEnclosingElement();
-					createClass(classElement, pckName);
-				} else if (implementatorElement.getKind() == ElementKind.CLASS) {
-					createClass(implementatorElement, pckName);
+					
+					TypeElement classElement = (TypeElement) implementatorElement.getEnclosingElement();
+					String classSimpleName = classElement.getSimpleName().toString();
+					String classQualifiedName = classElement.getQualifiedName().toString();
+					JavaFileObject jfo = null;
+					
+					try {
+						jfo = processingEnv.getFiler().createSourceFile(String.format("%s.%s%s", pckName , classSimpleName, CLASS_SUFFIX));
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+
+					BufferedWriter bw = null;
+					try {
+						bw = new BufferedWriter(jfo.openWriter());
+
+						bw.append(String.format("package %s;", pckName));
+						bw.newLine();bw.newLine();
+						
+						bw.append(String.format("import %s;", classQualifiedName));
+						bw.newLine();bw.newLine();
+						
+						
+						
+						bw.append(String.format("public class %s%s {", classSimpleName, CLASS_SUFFIX));
+						bw.newLine();bw.newLine();
+						
+						bw.append("\tstatic public void main(String[] args) {");
+						bw.newLine();
+						bw.append(String.format("\t\t(new %s%s()).foo();", classSimpleName, CLASS_SUFFIX));
+						bw.append("\t}");
+						bw.newLine();bw.newLine();
+						
+						bw.append("\tpublic void foo() {");
+						bw.newLine();
+						bw.append(String.format("\t\tSystem.out.println(\"I'm the wrapper %s%s for the %s class\");", classSimpleName, CLASS_SUFFIX, classSimpleName));
+						bw.newLine();
+						bw.append(String.format("\t\t(new %s()).foo();", classSimpleName));
+						bw.newLine();
+						bw.append("\t}");
+						bw.newLine();
+						bw.append("}");
+						
+						bw.flush();
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					} finally {
+						try {
+							bw.close();
+						} catch (IOException e) {
+						}
+					}
+				} else {
+					// The annotation support another Type than METHOD but no behavior was provided
 				}
-				
-				
 			}
 		}
 		
 		return true;
-	}
-
-	private void createClass(Element implementatorElement, String pckName) {
-		TypeElement classElement = (TypeElement) implementatorElement;
-		String classSimpleName = classElement.getSimpleName().toString();
-		JavaFileObject jfo = null;
-		
-		try {
-			jfo = processingEnv.getFiler().createSourceFile(String.format("%s.%s%s", pckName , classSimpleName, CLASS_SUFFIX));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		BufferedWriter bw = null;
-		try {
-			bw = new BufferedWriter(jfo.openWriter());
-
-			bw.append("package ");
-			bw.append(pckName + ";");
-			bw.newLine();
-			bw.append(String.format("public class %s%s {}", classSimpleName, CLASS_SUFFIX));
-			bw.newLine();
-
-			bw.flush();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} finally {
-			try {
-				bw.close();
-			} catch (IOException e) {
-			}
-		}
 	}
 }
