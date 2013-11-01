7fb74b26c2ddcf0dd6e8680b7359d79b4167e869
Gustavo Alberola <alberolagustavo@gmail.com>
2013-11-01 14:42:43 -0300

    Added factory path and class entry point

--- example/src/main/java/com/gustavoalberola/java/apt/example/AnnotatedClass.java
+++ example/src/main/java/com/gustavoalberola/java/apt/example/AnnotatedClass.java
@@ -4,8 +4,12 @@ import com.gustavoalberola.java.apt.annotation.SimpleAnnotation;
 
 public class AnnotatedClass {
 	
+	static public void main(String[] args) {
+		(new AnnotatedClass()).foo();
+	}
+	
 	@SimpleAnnotation
 	public void foo() {
-		System.out.println("Im the foo master");
+		System.out.println("Im the foo master");		
 	}
 }
